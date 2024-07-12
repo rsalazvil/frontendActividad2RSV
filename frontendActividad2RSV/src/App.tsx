610813f51f934c0344d287650e55989b1855ec29
unknown <rsalazvi@hotmail.es>
2024-07-12 10:58:38 +0200

    segundo commit

--- frontendActividad2RSV/src/App.tsx
+++ frontendActividad2RSV/src/App.tsx
@@ -1,11 +1,79 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
+import { abi } from "./assets/abis/CFETokenAbi";
+import { CFE_CONTRACT_ADDRESS } from "./constants";
+import { useState } from "react";
+import { waitForTransactionReceipt } from "wagmi/actions";
+import { config } from "./main";
+import { toast } from "react-toastify";
 
 function App() {
+  const { address, isConnected } = useAccount();
+  const [isMinting, setIsMinting] = useState(false);
+
+  const { data, isLoading, refetch } = useReadContract({
+    abi,
+    address: CFE_CONTRACT_ADDRESS,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  const { writeContractAsync } = useWriteContract();
+
+  const handleMint = async () => {
+    setIsMinting(true);
+
+    try {
+      const txHash = await writeContractAsync({
+        abi,
+        address: CFE_CONTRACT_ADDRESS,
+        functionName: "mint",
+        args: [address, 150],
+      });
+
+      await waitForTransactionReceipt(config, {
+        confirmations: 1,
+        hash: txHash,
+      });
+
+      setIsMinting(false);
+      toast.success("Minted 150 CFE tokens");
+      refetch();
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to mint CFE tokens");
+      setIsMinting(false);
+    }
+  };
+
   return (
-    
-    <main>
-      <h1 className="text-3xl font-bold text-red-500">hello</h1>
-      <ConnectButton />
+    <main className="w-full flex justify-center items-center min-h-svh flex-col">
+      <h1 className="text-4xl font-bold">🚀 CFE Token Faucet 🚀</h1>
+      <div className="my-5 p-4 flex flex-col gap-5 rounded border border-gray-300 items-center">
+        <ConnectButton />
+        {isConnected ? (
+          <div className="space-y-5">
+            <p>
+              💰 <span>Balance:</span>{" "}
+              {isLoading ? (
+                <span className="opacity-50">Loading...</span>
+              ) : (
+                data?.toString()
+              )}
+            </p>
+
+            <button
+              className="px-3 py-1 font-semibold bg-slate-700 rounded-xl disabled:opacity-50"
+              disabled={isMinting}
+              onClick={handleMint}
+            >
+              {isMinting ? "Minting..." : "Mint tokens"}
+            </button>
+          </div>
+        ) : (
+          <div>Please connect your wallet to use the faucet</div>
+        )}
+      </div>
     </main>
   );
 }
--- frontendActividad2RSV/src/main.tsx
+++ frontendActividad2RSV/src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { arbitrum, arbitrumSepolia } from "wagmi/chains";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
-
-const config = getDefaultConfig({
-  appName: "frontendActividad2RSV",
+export const config = getDefaultConfig({
+  appName: "Clase Frontend I",
   projectId: import.meta.env.VITE_PROJECT_ID,
   chains: [arbitrumSepolia, arbitrum],
 });
@@ -22,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <App />
+          <ToastContainer />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
